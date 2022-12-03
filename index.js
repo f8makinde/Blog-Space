55977ea3ca80235632c21cf4b79c8a563f7aa6c6
Faith Makinde <faithmakinde0@gmail.com>
2022-12-03 12:25:36 +0100

    completed

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+const postsArr = []
+const title = document.getElementById("title-blog")
+const body = document.getElementById("body-blog")
+
+function renderPosts(){
+  let html = ""
+  for (let post of postsArr) {
+      html += `
+          <h3>${post.title}</h3>
+          <p>${post.body}</p>
+          <hr />
+      `
+  }
+  document.getElementById("blog-list").innerHTML = html
+}
+fetch("https://jsonplaceholder.typicode.com/posts")
     .then(response => response.json())
     .then(data => {
-        const postsArr = data.slice(0, 5)
-        let html = ""
-        for (let post of postsArr) {
-            html += `
-                <h3>${post.title}</h3>
-                <p>${post.body}</p>
-                <hr />
-            `
-        }
-        document.getElementById("blog-list").innerHTML = html
+      postsArr = data.slice(0, 5)
+      renderPosts()
     })
 
 function formBtn(event){
   event.preventDefault()
-  const title = document.getElementById("title-blog").value
-  const body = document.getElementById("body-blog").value
+  const title = document.getElementById("title-blog")
+  const body = document.getElementById("body-blog")
 
   const data = {
-    title: title,
-    body: body
+    title: title.value,
+    body: body.value
   }
   const options = {
     method: "POST",
@@ -30,9 +37,15 @@ function formBtn(event){
     }
 }
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+fetch("https://jsonplaceholder.typicode.com/posts", options)
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(post => {
+       postsArr.unshift(post)
+       renderPosts()
+       title.value = ""
+       body.value = ""
+  })
+      
 }
 
 document.getElementById("form-btn").addEventListener('submit', formBtn)
