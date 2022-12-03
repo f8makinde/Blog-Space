3e297e48737ebf809854ec64e31daca0e843e379
Faith Makinde <faithmakinde0@gmail.com>
2022-12-03 11:12:29 +0100

    sent new posts to the server

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-fetch('https://apis.scrimba.com/jsonplaceholder/posts')
-.then(response => response.json())
-.then(data => {
-    const postArr = data.slice(0, 5)
-    let html = ""
-    for(post of postArr){
-           html +=
-         `
-          <h3>${post.title}</h3>
-          <p>${post.body}</p>
-          <hr/>
-        `   
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then(response => response.json())
+    .then(data => {
+        const postsArr = data.slice(0, 5)
+        let html = ""
+        for (let post of postsArr) {
+            html += `
+                <h3>${post.title}</h3>
+                <p>${post.body}</p>
+                <hr />
+            `
+        }
+        document.getElementById("blog-list").innerHTML = html
+    })
+
+function formBtn(event){
+  event.preventDefault()
+  const title = document.getElementById("title-blog").value
+  const body = document.getElementById("body-blog").value
+
+  const data = {
+    title: title,
+    body: body
+  }
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+        "Content-Type": "application/json"
     }
-    document.getElementById("blog-list").innerHTML = html;
-})
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+document.getElementById("form-btn").addEventListener('submit', formBtn)
